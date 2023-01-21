839f81f04f1678f0bce39bde6ab2083b2fcfb70c
Shubham Pandey <shubhampandey@Shubhams-MacBook-Air.local>
2023-01-21 15:29:20 +0530

    incorporated commnets

--- subarray_with_given_sum.py
+++ subarray_with_given_sum.py
@@ -9,6 +9,9 @@ def subarray_with_given_sum(array, summation):
             break
     return index
 
+def main():
+    ar = [1,2,3,7,5]
+    print(subarray_with_given_sum(ar, 10))
 
-ar = [1,2,3,7,5]
-print(subarray_with_given_sum(ar, 10))
+if __name__ == "__main__":
+    main()
