025264716d4b122a6e51159b97a0e84bc007d048
Shubham Pandey <shubhampandey@Shubhams-MacBook-Air.local>
2023-01-21 15:26:45 +0530

    added subarray_with_sum problem

---
+++ subarray_with_given_sum.py
@@ -0,0 +1,14 @@
+def subarray_with_given_sum(array, summation):
+    index = -1
+    for i in range(len(array)):
+        for j in range(i+1,len(array)+1):
+            if sum(array[i:j]) == summation:
+                index = [i+1, j]
+                break
+        if index != -1:
+            break
+    return index
+
+
+ar = [1,2,3,7,5]
+print(subarray_with_given_sum(ar, 10))
