36bbecfa04c9641da2a42991abe8119b3d5cbae3
Shubham Pandey <shubhampandey@Shubhams-MacBook-Air.local>
2023-01-27 11:30:05 +0530

    added function to reverse linkedlist, refactored code

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -4,14 +4,18 @@ from node import Node
 class LinkedList(object):
     def __init__(self):
         self.head = None
+        self.current = None
+
     def create_list(self, data):
         for index, val in enumerate(data):
-            print("adding element {} to linkedlist".format(val))
+            print("adding element {} to linked-list".format(val))
+            node = Node(val)
             if index == 0:
-                self.head = Node(val)
-                copy = self.head
-            copy.next = Node(val)
-            copy = copy.next
+                self.head = node                
+            else:
+                self.current.next = node
+            self.current = node
+
 
     def traverse_list(self):
         print("Traversing linkedlist..")
