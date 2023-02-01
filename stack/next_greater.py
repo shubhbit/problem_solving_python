f0c8638eff522feb370a0a7f7031827747379c00
Shubham Pandey <shubhampandey@Shubhams-MacBook-Air.local>
2023-02-01 10:49:42 +0530

    added solution to find next greater element in array for each element

---
+++ stack/next_greater.py
@@ -0,0 +1,21 @@
+
+def next_greater(ar):
+    st = ""
+    for indx, i in enumerate(ar):
+        for j in ar[indx+1:]:
+            if j > i:
+                st += str(j)
+                break
+    st += "-1"
+    print(type(st))
+    return " ".join(list(st))
+
+
+
+if __name__ == "__main__":
+    # Input: 
+    # N = 4, arr[] = [1 3 2 4]
+    # Output:
+    # 3 4 4 -1
+    ar = [1, 3, 2, 4]
+    print(next_greater(ar))
