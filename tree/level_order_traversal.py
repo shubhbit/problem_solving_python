accf587d144972d9afacd91e17703c63ddc927c2
Shubham Pandey <shubhampandey@Shubhams-MacBook-Air.local>
2023-02-06 17:58:17 +0530

    level-order-traversal

---
+++ tree/level_order_traversal.py
@@ -0,0 +1,21 @@
+####################################################################################
+#                                [50]
+#                             /        \
+#                         [20]          [45]
+#                        /    \        /    \
+#                    [11]      [15]  [30]    [78]
+# Output:
+# 50
+# 20 45
+# 11 15 30 78
+# level order traversal is also known as breadth first traversal
+####################################################################################
+
+
+from tree import implement_tree
+
+def level_order_traversal(root):
+    pass
+
+if __name__ == "__main__":
+    pass
